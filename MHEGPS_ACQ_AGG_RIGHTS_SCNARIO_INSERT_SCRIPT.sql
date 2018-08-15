807cecea7ba4582a6e4c4d0ef5ab0821ebf92c67
SouravDas1234 <42412164+SouravDas1234@users.noreply.github.com>
2018-08-15 20:49:08 +0530

    Add files via upload

---
+++ MHEGPS_ACQ_AGG_RIGHTS_SCNARIO_INSERT_SCRIPT.sql
@@ -0,0 +1,164 @@
+/***************************************************************
+/*    Name: MHE_GPS_ACQ_AGG_RIGHTS_SCNARIO
+/*    Object Type: Insert script
+/*    Description: This is an SQL script for inserting data in Sequence table in MHEETL schema
+/*
+/* Change History:
+/* Date         Name                   IR   Modification
+/* -----------  -------------------    ---  ------------
+/* 01/15/2018   TCS                    1.0    Create
+/***************************************************************/
+
+
+INSERT INTO mhe_gps_acq_agg_rights_scnario VALUES(mhe_gps_acq_agg_rgts_seq_s.nextval,'SCENERIO-1','Single New Contract per Rightsholder Vendor with the status of either Booked or Processed
+Single Vendor with Individual Rights
+Rights Category for the asset is either Rights Managed or Royalty Free',SYSDATE,'GPSAPPL',SYSDATE,'GPSAPPL');
+INSERT INTO mhe_gps_acq_agg_rights_scnario VALUES(mhe_gps_acq_agg_rgts_seq_s.nextval,'SCENERIO-2','Multiple New Contracts per Rightsholder Vendor with the status of either Booked or Processed
+Single Vendor with Individual Rights
+Rights Category for the asset is either Rights Managed or Royalty Free',SYSDATE,'GPSAPPL',SYSDATE,'GPSAPPL');
+INSERT INTO mhe_gps_acq_agg_rights_scnario VALUES(mhe_gps_acq_agg_rgts_seq_s.nextval,'SCENERIO-3','Single New Contract per Rightsholder Vendor with the status of either Booked or Processed
+Multiple Vendors with Individual Rights
+Rights Category for the asset is either Rights Managed or Royalty Free',SYSDATE,'GPSAPPL',SYSDATE,'GPSAPPL');
+INSERT INTO mhe_gps_acq_agg_rights_scnario VALUES(mhe_gps_acq_agg_rgts_seq_s.nextval,'SCENERIO-4','Multiple New Contracts per Rightsholder Vendor with the status of either Booked or Processed
+Multiple Vendors with Individual Rights
+Rights Category for the asset is either Rights Managed or Royalty Free',SYSDATE,'GPSAPPL',SYSDATE,'GPSAPPL');
+INSERT INTO mhe_gps_acq_agg_rights_scnario VALUES(mhe_gps_acq_agg_rgts_seq_s.nextval,'SCENERIO-5','Single New Contract per Rightsholder Vendor with the status of either Booked or Processed
+Single/Multiple Extension Contracts (the extension is only for duration) with the status of either Booked or Processed
+Single Vendor with Individual Rights
+Rights Category for the asset is either Rights Managed or Royalty Free',SYSDATE,'GPSAPPL',SYSDATE,'GPSAPPL');
+INSERT INTO mhe_gps_acq_agg_rights_scnario VALUES(mhe_gps_acq_agg_rgts_seq_s.nextval,'SCENERIO-5.1','Single New Contract per Rightsholder Vendor with the status of either Booked or Processed
+Single/Multiple Extension Contracts with the status of either Booked or Processed
+Single Vendor with Individual Rights
+Rights Category for the asset is either Rights Managed or Royalty Free',SYSDATE,'GPSAPPL',SYSDATE,'GPSAPPL');
+INSERT INTO mhe_gps_acq_agg_rights_scnario VALUES(mhe_gps_acq_agg_rgts_seq_s.nextval,'SCENERIO-6','Multiple New Contract per Rightsholder Vendor with the status of either Booked or Processed
+Single/Multiple Extension Contracts (the extension is only for duration) with the status of either Booked or Processed
+Single Vendor with Individual Rights
+Rights Category for the asset is either Rights Managed or Royalty Free',SYSDATE,'GPSAPPL',SYSDATE,'GPSAPPL');
+INSERT INTO mhe_gps_acq_agg_rights_scnario VALUES(mhe_gps_acq_agg_rgts_seq_s.nextval,'SCENERIO-6.1','Multiple New Contract per Rightsholder Vendor with the status of either Booked or Processed
+Single/Multiple Extension Contracts with the status of either Booked or Processed
+Single Vendor with Individual Rights
+Rights Category for the asset is either Rights Managed or Royalty Free',SYSDATE,'GPSAPPL',SYSDATE,'GPSAPPL');
+INSERT INTO mhe_gps_acq_agg_rights_scnario VALUES(mhe_gps_acq_agg_rgts_seq_s.nextval,'SCENERIO-7','Single New Contract per Rightsholder Vendor with the status of either Booked or Processed
+Single/Multiple Extension Contracts (the extension is only for duration) with the status of either Booked or Processed
+Multiple Vendors with Individual Rights
+Rights Category for the asset is either Rights Managed or Royalty Free',SYSDATE,'GPSAPPL',SYSDATE,'GPSAPPL');
+INSERT INTO mhe_gps_acq_agg_rights_scnario VALUES(mhe_gps_acq_agg_rgts_seq_s.nextval,'SCENERIO-7.1','Single New Contract per Rightsholder Vendor with the status of either Booked or Processed
+Single/Multiple Extension Contracts with the status of either Booked or Processed
+Multiple Vendors with Individual Rights
+Rights Category for the asset is either Rights Managed or Royalty Free',SYSDATE,'GPSAPPL',SYSDATE,'GPSAPPL');
+INSERT INTO mhe_gps_acq_agg_rights_scnario VALUES(mhe_gps_acq_agg_rgts_seq_s.nextval,'SCENERIO-8','Multiple New Contract per Rightsholder Vendor with the status of either Booked or Processed
+Single/Multiple Extension Contracts (the extension is only for duration) with the status of either Booked or Processed
+Multiple Vendors with Individual Rights
+Rights Category for the asset is either Rights Managed or Royalty Free',SYSDATE,'GPSAPPL',SYSDATE,'GPSAPPL');
+INSERT INTO mhe_gps_acq_agg_rights_scnario VALUES(mhe_gps_acq_agg_rgts_seq_s.nextval,'SCENERIO-8.1','Multiple New Contract per Rightsholder Vendor with the status of either Booked or Processed
+Single/Multiple Extension Contracts  with the status of either Booked or Processed
+Multiple Vendors with Individual Rights
+Rights Category for the asset is either Rights Managed or Royalty Free',SYSDATE,'GPSAPPL',SYSDATE,'GPSAPPL');
+INSERT INTO mhe_gps_acq_agg_rights_scnario VALUES(mhe_gps_acq_agg_rgts_seq_s.nextval,'SCENERIO-9','Single New Contract per Rightsholder Vendor (with only one attribute with different value) with the status of either Booked or Processed
+Multiple Vendors with Combined Rights, single group
+Rights Category for the asset is either Rights Managed or Royalty Free',SYSDATE,'GPSAPPL',SYSDATE,'GPSAPPL');
+INSERT INTO mhe_gps_acq_agg_rights_scnario VALUES(mhe_gps_acq_agg_rgts_seq_s.nextval,'SCENERIO-9.1','Single New Contract per Rightsholder Vendor with the status of either Booked or Processed
+Multiple Vendors with Combined Rights, single group
+Rights Category for the asset is either Rights Managed or Royalty Free',SYSDATE,'GPSAPPL',SYSDATE,'GPSAPPL');
+INSERT INTO mhe_gps_acq_agg_rights_scnario VALUES(mhe_gps_acq_agg_rgts_seq_s.nextval,'SCENERIO-10','Single New Contract per Rightsholder Vendor (with only one attribute with different value) with the status of either Booked or Processed
+Multiple Vendors with Combined Rights
+Single/Multipe Vendors with Individual Rights
+Rights Category for the asset is either Rights Managed or Royalty Free',SYSDATE,'GPSAPPL',SYSDATE,'GPSAPPL');
+INSERT INTO mhe_gps_acq_agg_rights_scnario VALUES(mhe_gps_acq_agg_rgts_seq_s.nextval,'SCENERIO-10.1','Single New Contract per Rightsholder Vendor with the status of either Booked or Processed
+Multiple Vendors with Combined Rights
+Single/Multipe Vendors with Individual Rights
+Rights Category for the asset is either Rights Managed or Royalty Free',SYSDATE,'GPSAPPL',SYSDATE,'GPSAPPL');
+INSERT INTO mhe_gps_acq_agg_rights_scnario VALUES(mhe_gps_acq_agg_rgts_seq_s.nextval,'SCENERIO-11','Single New Contract per Rightsholder Vendor (with only one attribute with different value) with the status of either Booked or Processed
+Multiple Vendors with Combined Rights, multiple groups
+Rights Category for the asset is either Rights Managed or Royalty Free',SYSDATE,'GPSAPPL',SYSDATE,'GPSAPPL');
+INSERT INTO mhe_gps_acq_agg_rights_scnario VALUES(mhe_gps_acq_agg_rgts_seq_s.nextval,'SCENERIO-11.1','Single New Contract per Rightsholder Vendor with the status of either Booked or Processed
+Multiple Vendors with Combined Rights, multiple groups
+Rights Category for the asset is either Rights Managed or Royalty Free',SYSDATE,'GPSAPPL',SYSDATE,'GPSAPPL');
+INSERT INTO mhe_gps_acq_agg_rights_scnario VALUES(mhe_gps_acq_agg_rgts_seq_s.nextval,'SCENERIO-12','Multiple New Contract per Rightsholder Vendor with the status of either Booked or Processed
+Multiple Vendors with Combined Rights, single group
+Rights Category for the asset is either Rights Managed or Royalty Free',SYSDATE,'GPSAPPL',SYSDATE,'GPSAPPL');
+INSERT INTO mhe_gps_acq_agg_rights_scnario VALUES(mhe_gps_acq_agg_rgts_seq_s.nextval,'SCENERIO-13','Single New Contract per Rightsholder Vendor (with only one attribute with different value) with the status of either Booked or Processed
+Single/Multiple Extension Contracts (the extension is only for duration) with the status of either Booked or Processed
+Multiple Vendors with Combined Rights, single group
+Rights Category for the asset is either Rights Managed or Royalty Free',SYSDATE,'GPSAPPL',SYSDATE,'GPSAPPL');
+INSERT INTO mhe_gps_acq_agg_rights_scnario VALUES(mhe_gps_acq_agg_rgts_seq_s.nextval,'SCENERIO-13.1','Single New Contract per Rightsholder Vendor with the status of either Booked or Processed
+Single/Multiple Extension Contracts (the extension is only for duration) with the status of either Booked or Processed
+Multiple Vendors with Combined Rights, single group
+Rights Category for the asset is either Rights Managed or Royalty Free',SYSDATE,'GPSAPPL',SYSDATE,'GPSAPPL');
+INSERT INTO mhe_gps_acq_agg_rights_scnario VALUES(mhe_gps_acq_agg_rgts_seq_s.nextval,'SCENERIO-13.2','Single New Contract per Rightsholder Vendor with the status of either Booked or Processed
+Single/Multiple Extension Contracts with the status of either Booked or Processed
+Multiple Vendors with Combined Rights, single group
+Rights Category for the asset is either Rights Managed or Royalty Free',SYSDATE,'GPSAPPL',SYSDATE,'GPSAPPL');
+INSERT INTO mhe_gps_acq_agg_rights_scnario VALUES(mhe_gps_acq_agg_rgts_seq_s.nextval,'SCENERIO-14','Single New Contract per Rightsholder Vendor (with only one attribute with different value) with the status of either Booked or Processed
+Multiple Vendors with Combined Rights
+Single/Multiple Extension Contracts with the status of either Booked or Processed
+Single/Multipe Vendors with Individual Rights
+Rights Category for the asset is either Rights Managed or Royalty Free',SYSDATE,'GPSAPPL',SYSDATE,'GPSAPPL');
+INSERT INTO mhe_gps_acq_agg_rights_scnario VALUES(mhe_gps_acq_agg_rgts_seq_s.nextval,'SCENERIO-15','Single New Contract per Rightsholder Vendor (with only one attribute with different value) with the status of either Booked or Processed
+Multiple Vendors with Combined Rights, multiple groups
+Single/Multiple Extension Contracts (the extension is only for duration) with the status of either Booked or Processed
+Rights Category for the asset is either Rights Managed or Royalty Free',SYSDATE,'GPSAPPL',SYSDATE,'GPSAPPL');
+INSERT INTO mhe_gps_acq_agg_rights_scnario VALUES(mhe_gps_acq_agg_rgts_seq_s.nextval,'SCENERIO-15.1','Single New Contract per Rightsholder Vendor with the status of either Booked or Processed
+Multiple Vendors with Combined Rights, multiple groups
+Single/Multiple Extension Contracts (the extension is only for duration) with the status of either Booked or Processed
+Rights Category for the asset is either Rights Managed or Royalty Free',SYSDATE,'GPSAPPL',SYSDATE,'GPSAPPL');
+INSERT INTO mhe_gps_acq_agg_rights_scnario VALUES(mhe_gps_acq_agg_rgts_seq_s.nextval,'SCENERIO-15.2','Single New Contract per Rightsholder Vendor with the status of either Booked or Processed
+Multiple Vendors with Combined Rights, multiple groups
+Single/Multiple Extension Contracts with the status of either Booked or Processed
+Rights Category for the asset is either Rights Managed or Royalty Free',SYSDATE,'GPSAPPL',SYSDATE,'GPSAPPL');
+INSERT INTO mhe_gps_acq_agg_rights_scnario VALUES(mhe_gps_acq_agg_rgts_seq_s.nextval,'SCENERIO-16','Multiple New Contract per Rightsholder Vendor with the status of either Booked or Processed
+Multiple Vendors with Combined Rights, single group
+Single/Multiple Extension Contracts (the extension is only for duration) with the status of either Booked or Processed
+Rights Category for the asset is either Rights Managed or Royalty Free',SYSDATE,'GPSAPPL',SYSDATE,'GPSAPPL');
+INSERT INTO mhe_gps_acq_agg_rights_scnario VALUES(mhe_gps_acq_agg_rgts_seq_s.nextval,'SCENERIO-16.1','Multiple New Contract per Rightsholder Vendor with the status of either Booked or Processed
+Multiple Vendors with Combined Rights, single group
+Single/Multiple Extension Contracts with the status of either Booked or Processed
+Rights Category for the asset is either Rights Managed or Royalty Free',SYSDATE,'GPSAPPL',SYSDATE,'GPSAPPL');
+INSERT INTO mhe_gps_acq_agg_rights_scnario
+(acq_agg_rights_scenario_sys_id,scenario_id,scenario_desc,creation_date,creation_user_id,last_update_date,last_update_user_id)
+VALUES (-99,'Default','Unit Limit Aggregation',
+SYSDATE,
+(SELECT person_sys_id FROM mhe_gps_person WHERE person_first_name = 'INTEGRATION_USER'),
+SYSDATE,
+(SELECT person_sys_id FROM mhe_gps_person WHERE person_first_name = 'INTEGRATION_USER')
+);
+
+INSERT INTO mhe_gps_acq_agg_rights_scnario
+(acq_agg_rights_scenario_sys_id,scenario_id,scenario_desc,creation_date,creation_user_id,last_update_date,last_update_user_id)
+VALUES (-98,'Default','Component Level Aggregation',
+SYSDATE,
+(SELECT person_sys_id FROM mhe_gps_person WHERE person_first_name = 'INTEGRATION_USER'),
+SYSDATE,
+(SELECT person_sys_id FROM mhe_gps_person WHERE person_first_name = 'INTEGRATION_USER')
+);
+
+INSERT INTO mhe_gps_acq_agg_rights_scnario
+(acq_agg_rights_scenario_sys_id,scenario_id,scenario_desc,creation_date,creation_user_id,last_update_date,last_update_user_id)
+VALUES (-97,'Default','Parent Component Level Aggregation',
+SYSDATE,
+(SELECT person_sys_id FROM mhe_gps_person WHERE person_first_name = 'INTEGRATION_USER'),
+SYSDATE,
+(SELECT person_sys_id FROM mhe_gps_person WHERE person_first_name = 'INTEGRATION_USER')
+);
+
+INSERT INTO mhe_gps_acq_agg_rights_scnario
+(acq_agg_rights_scenario_sys_id,scenario_id,scenario_desc,creation_date,creation_user_id,last_update_date,last_update_user_id)
+VALUES (-96,'Default','Product Level Aggregation',
+SYSDATE,
+(SELECT person_sys_id FROM mhe_gps_person WHERE person_first_name = 'INTEGRATION_USER'),
+SYSDATE,
+(SELECT person_sys_id FROM mhe_gps_person WHERE person_first_name = 'INTEGRATION_USER')
+);
+
+INSERT INTO mhe_gps_acq_agg_rights_scnario
+(acq_agg_rights_scenario_sys_id,scenario_id,scenario_desc,creation_date,creation_user_id,last_update_date,last_update_user_id)
+VALUES (-95,'Default','Node Level Aggregation',
+SYSDATE,
+(SELECT person_sys_id FROM mhe_gps_person WHERE person_first_name = 'INTEGRATION_USER'),
+SYSDATE,
+(SELECT person_sys_id FROM mhe_gps_person WHERE person_first_name = 'INTEGRATION_USER')
+);
+
+insert into mhe_gps_acq_agg_rights_scnario values 
+(0,'Default Asset','Scenario Not Found',SYSDATE,'GPSAPPL',SYSDATE,'GPSAPPL');
+commit;
